a54f1b1d8c335f87ae16bac4ae31ed26af7b9170
Dean Shabi <dean@tem.energy>
2025-04-25 12:47:46 +0200

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import VantaScripts from '@/components/VantaScripts';
+import { Analytics } from "@vercel/analytics/react";
 
 const outfit = Outfit({ 
   subsets: ['latin'], 
@@ -50,6 +51,7 @@ export default function RootLayout({
         <Footer />
 
         <VantaScripts />
+        <Analytics />
       </body>
     </html>
   );
